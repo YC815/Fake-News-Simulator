da35846dc29fbea99e1cd629d8b5799199f468df
YC815 <minnecraft.YC815@gmail.com>
2023-10-07 16:13:46 +0800

    first commit

---
+++ src/components/theme-provider.jsx
@@ -0,0 +1,10 @@
+"use client"
+// 主題顏色組件
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
